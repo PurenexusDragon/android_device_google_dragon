1f8296d5bf06163ff3184d465b4b382ab2c6ac51
Josh Chasky <jchasky@gmail.com>
2016-11-10 14:13:31 -0500

    Dragon: Fix NRD91N build fingerprint
    Change-Id: I00fd3d4e2739225dddc02d7e1b8921169b091c66

--- du.mk
+++ du.mk
@@ -32,5 +32,5 @@ PRODUCT_NAME := du_dragon
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=dragon \
-    BUILD_FINGERPRINT=google/ryu/dragon:7.0/NRD91N/3318108:user/release-keys \
-    PRIVATE_BUILD_DESC="ryu-user 7.0 NRD91N 3318108 release-keys"
+    BUILD_FINGERPRINT=google/ryu/dragon:7.0/NRD91N/3318108:userdebug/dev-keys \
+    PRIVATE_BUILD_DESC="ryu-user 7.0 NRD91N 3318108 dev-keys"
